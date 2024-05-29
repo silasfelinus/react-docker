17ef63cf450da000ae233973f692685271aa4c64
Silas Knight <silasfelinus@gmail.com>
2024-05-29 05:41:43 -0700

    eslint typescript prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
+import React from "react";
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
